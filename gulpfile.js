653024425769f103d06b224676c346018fc6dc49
Alexander Yaroslavtsev <a1exweb@mail.ru>
2019-12-01 13:54:50 +0300

    Add css-minify task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
 gulp.task('browser-sync', function () {
     browserSync.init({
@@ -8,4 +10,11 @@ gulp.task('browser-sync', function () {
         }
     });
     gulp.watch("./*.*").on('change', browserSync.reload);
+});
+
+gulp.task('css-minify', function () {
+    gulp.src('./**/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'));
 });
