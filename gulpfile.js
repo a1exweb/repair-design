a8a2f27d8f8431a99c85d924361971825a2f77e8
Alexander Yaroslavtsev <a1exweb@mail.ru>
2019-12-02 22:54:52 +0300

    fix: Add cssmin in new version gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
 function bs() {
     serveSass();
+    cssMin();
     browserSync.init({
         server: {
             baseDir: "./"
@@ -19,5 +22,14 @@ function serveSass() {
         .pipe(dest("./css"))
         .pipe(browserSync.stream());
 };
+
+function cssMin() {
+    src('./css/*.css')
+        .pipe(cssmin())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(dest('./css'));
+}
 
 exports.serve = bs;
