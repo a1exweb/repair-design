3fe04d4e25f734dc33be5f7015b9ca6bb0df6767
Alexander Yaroslavtsev <a1exweb@mail.ru>
2019-12-13 20:50:52 +0300

    feat: add swiper add section projects

--- js/main.js
+++ js/main.js
@@ -62,4 +62,22 @@ $(document).ready(function () {
         }, 500);
         return false;
     });
+
+    //initialize swiper when document ready
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+    var next = $('.swiper-button-next');
+    var prev = $('.swiper-button-prev');
+    var bullets = $('.swiper-pagination');
+    next.css('left', prev.width() + 10 + bullets.width() + 10);
+    bullets.css('left', prev.width() + 10);
 });
